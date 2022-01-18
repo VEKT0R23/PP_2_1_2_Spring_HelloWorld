36ed425b81d76fff1f7cce18aa145b0aa76612e7
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    fix pom

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("catBean");
+        Cat cat2 = (Cat) applicationContext.getBean("catBean");
+
+        System.out.println("Переменные класса Cat ссылаются на один и тот же объект? " + (bean1 == bean2));
+        System.out.println("Переменные класса Cat ссылаются на один и тот же объект? " + (cat1 == cat2));
+
     }
 }
